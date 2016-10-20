7d91bc50d2823116bba98efab380802bca38d5c8
Andrew Holland <aholland@a1dutch.co.uk>
2016-10-20 10:15:08 +0100

    initial commit

---
+++ src/main/java/uk/co/a1dutch/crawler/betfair/SurebetCrawlerBetfairApplication.java
@@ -0,0 +1,12 @@
+package uk.co.a1dutch.crawler.betfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurebetCrawlerBetfairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurebetCrawlerBetfairApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/co/a1dutch/crawler/betfair/SurebetCrawlerBetfairApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.a1dutch.crawler.betfair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SurebetCrawlerBetfairApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
